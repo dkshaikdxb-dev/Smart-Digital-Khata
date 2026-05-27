1740b9925c75354caad000dde9ba5d919575cbd0
dkshaikdxb-dev <dk.shaikdxb@gmail.com>
2026-05-27 18:48:03 +0530

    Register notification routes

--- backend/api/src/routes/index.js
+++ backend/api/src/routes/index.js
@@ -6,11 +6,13 @@ const customerRoutes = require('./customer.routes');
 const ledgerRoutes = require('./ledger.routes');
 const shopRoutes = require('./shop.routes');
 const dashboardRoutes = require('./dashboard.routes');
+const notificationRoutes = require('./notification.routes');
 
 router.use('/auth', authRoutes);
 router.use('/customers', customerRoutes);
 router.use('/ledger', ledgerRoutes);
 router.use('/shops', shopRoutes);
 router.use('/dashboard', dashboardRoutes);
+router.use('/notifications', notificationRoutes);
 
 module.exports = router;
