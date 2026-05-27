bafc21454df8ea495806e5e6208f28a1953f3e7c
dkshaikdxb-dev <dk.shaikdxb@gmail.com>
2026-05-27 18:20:54 +0530

    Register shop routes

--- backend/api/src/routes/index.js
+++ backend/api/src/routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const authRoutes = require('./auth.routes');
 const customerRoutes = require('./customer.routes');
 const ledgerRoutes = require('./ledger.routes');
+const shopRoutes = require('./shop.routes');
 
 router.use('/auth', authRoutes);
 router.use('/customers', customerRoutes);
 router.use('/ledger', ledgerRoutes);
+router.use('/shops', shopRoutes);
 
 module.exports = router;
