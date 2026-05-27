6e08e1d8f7d617fd49a1366a1d4160876c742f5e
dkshaikdxb-dev <dk.shaikdxb@gmail.com>
2026-05-27 18:21:38 +0530

    Add customer validation middleware to routes

--- backend/api/src/routes/customer.routes.js
+++ backend/api/src/routes/customer.routes.js
@@ -3,9 +3,15 @@ const router = express.Router();
 
 const authMiddleware = require('../middleware/auth.middleware');
 const customerController = require('../controllers/customer.controller');
+const { validateCustomerPayload } = require('../validators/customer.validator');
 
 router.get('/', authMiddleware, customerController.getCustomers);
 
-router.post('/', authMiddleware, customerController.createCustomer);
+router.post(
+  '/',
+  authMiddleware,
+  validateCustomerPayload,
+  customerController.createCustomer
+);
 
 module.exports = router;
