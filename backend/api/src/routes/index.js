19439b50a840ccb1983089791eac38186414404e
dkshaikdxb-dev <dk.shaikdxb@gmail.com>
2026-05-27 18:42:29 +0530

    Register dashboard routes

--- backend/api/src/routes/index.js
+++ backend/api/src/routes/index.js
@@ -5,10 +5,12 @@ const authRoutes = require('./auth.routes');
 const customerRoutes = require('./customer.routes');
 const ledgerRoutes = require('./ledger.routes');
 const shopRoutes = require('./shop.routes');
+const dashboardRoutes = require('./dashboard.routes');
 
 router.use('/auth', authRoutes);
 router.use('/customers', customerRoutes);
 router.use('/ledger', ledgerRoutes);
 router.use('/shops', shopRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 module.exports = router;
